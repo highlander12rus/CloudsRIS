a5a24e70753ef8b25cc42aeb799bd96112e7e813
Marina Mezentseva <marina-521@mail.ru>
2013-12-03 23:44:17 +0600

    Добавлены файлы Folder.cs и функциональные тесты

---
+++ Client/Client/Classes/Folder.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Client.Interfaces;
+using Newtonsoft.Json;
+
+namespace Client.Classes
+{
+    public class Folder : IFolder
+    {
+        private const string URL_API_FOLDER = @"http://90.157.18.190/folder/";
+
+        public bool CreateFolder(string path,string token)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Post, URL_API_FOLDER);
+                request.Headers.Add("Authorization", "Basic " + token);
+                request.Headers.Add("Content-Type", "application/x-www-form-urlencoded ");
+
+                var response = client.SendAsync(request);
+                var result = response.Result.Content.ReadAsStringAsync();
+
+                //dynamic jsonAnswer = JsonConvert.DeserializeObject(result.Result);
+                dynamic jsonAnswer = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(result.Result);
+
+
+
+                return jsonAnswer;//.folders+"!"+jsonAnswer.files;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool ChangeFolderName(string name, string new_name, string token)
+        {
+            return true;
+        }
+
+        public bool DeleteFolder(string path, string token)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, URL_API_FOLDER + "?folder=" + path);
+                request.Headers.Add("Authorization", "Basic " + token);
+
+                var response = client.SendAsync(request);
+                var result = response.Result.Content.ReadAsStringAsync();
+
+                //dynamic jsonAnswer = JsonConvert.DeserializeObject(result.Result);
+                dynamic jsonAnswer = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(result.Result);
+
+
+
+                return jsonAnswer;//.folders+"!"+jsonAnswer.files;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Dictionary<string, List<string>> GetFolderContent(string path, string token)
+        {
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, URL_API_FOLDER+"?folder="+path);
+                request.Headers.Add("Authorization", "Basic " + token);
+
+                var response = client.SendAsync(request);
+                var result = response.Result.Content.ReadAsStringAsync();
+
+                //dynamic jsonAnswer = JsonConvert.DeserializeObject(result.Result);
+                dynamic jsonAnswer = JsonConvert.DeserializeObject<Dictionary<string,List<string>>>(result.Result);
+                
+
+               
+                return jsonAnswer;//.folders+"!"+jsonAnswer.files;
+            }
+            catch
+            {
+                Dictionary<string,List<string>> errors = new Dictionary<string, List<string>>();
+                List<string> error = new List<string>();
+                error.Add("Запрашиваемая папка недоступна или не найдена");
+                errors.Add("error",l );
+                return errors;
+            }
+        }
+        
+    }
+}
---
+++ Client/TestClient/TestClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Client.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace TestClient
+{
+    [TestClass]
+    public class TestClient
+    {
+        [TestMethod]
+        public void TestAuthorize()
+        {
+            var user = new User();
+            var token = user.Authorize("ilia12r@gmail.com", "ilya");//FB4579611A1C7831F69AEBF64363D69EF69F8D9B74C9FE595448403542A43D4367EC727A01D0DB7DA9245E12373C105D6E6CEBEB73DBD70B6F7E5A13E0CF8C9A
+            Assert.AreNotEqual("Логин или пароль неверны",token);
+        }
+
+        [TestMethod]
+        public void TestGetFolderContent()
+        {
+            var user = new User();
+            var folder = new Folder();
+
+            var token = user.Authorize("ilia12r@gmail.com", "ilya");
+            var folderContent = folder.GetFolderContent("/", token);
+            if (folderContent.ContainsKey("error")) Assert.Fail("Запрашиваемая папка недоступна или не найдена");
+            if (folderContent.ContainsKey("folders")) Assert.AreEqual(0,folderContent["folders"].Count);
+            if (folderContent.ContainsKey("files")) Assert.AreEqual(0, folderContent["files"].Count);
+        }
+
+    }
+}
